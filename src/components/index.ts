b92d849f455c73379a9a6908cc62df87100fd8cc
mahdaoui <a.mahdaoui@sanadtech.com>
2023-02-27 09:41:01 +0100

    feat : Update config files to use vite to build npm package

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import FilterField from './filterField/FilterField';
+import './filterField/filterField.css';
+
+export type { FilterFieldProps } from './filterField/types';
+export default FilterField;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+import * as packageJson from './package.json';
+// import EsLint from 'vite-plugin-linter';
+// const { EsLinter, linterPlugin } = EsLint;
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default defineConfig((configEnv) => ({
+  plugins: [
+    dts({
+      include: ['src/components/'],
+    }),
+    react(),
+    tsConfigPaths(),
+    // linterPlugin({
+    //   include: ['./src}/**/*.{ts,tsx}'],
+    //   linters: [new EsLinter({ configEnv })],
+    // }),
+  ],
+  build: {
+    lib: {
+      entry: resolve('src', 'components/index.ts'),
+      name: 'ReactViteLibrary',
+      formats: ['es', 'umd'],
+      fileName: (format) => `react-mui-filter.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+    },
+  },
+}));
